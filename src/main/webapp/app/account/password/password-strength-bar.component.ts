68beaf1ff2c1d265604fbed4ad57cc641996110e
Marcel Jonas <mjonas@oakland.edu>
2019-04-04 23:48:31 -0400

    altered the loop condition for stricter strenght bar

--- src/main/webapp/app/account/password/password-strength-bar.component.ts
+++ src/main/webapp/app/account/password/password-strength-bar.component.ts
@@ -73,7 +73,7 @@ export class PasswordStrengthBarComponent {
                 this.renderer.setElementClass(element, element.className, false);
             }
             const lis = element.getElementsByTagName('li');
-            for (let i = 0; i < lis.length; i++) {
+            for (let i = 0; i < 6; i++) {
                 if (i < c.idx) {
                     this.renderer.setElementStyle(lis[i], 'backgroundColor', c.col);
                 } else {
